a64503c953978c31dffd17483d7ad5f76851a9b2
gensisFA <gefarellano@gmail.com>
2026-05-28 15:06:29 -0600

    feat: conexion a la pagina de sinee

--- backend/src/usuarios/usuarios.controller.ts
+++ backend/src/usuarios/usuarios.controller.ts
@@ -1,4 +1,30 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post, Body } from '@nestjs/common';
+import { UsuariosService } from './usuarios.service';
 
 @Controller('usuarios')
-export class UsuariosController {}
+export class UsuariosController {
+  constructor(private readonly usuariosService: UsuariosService) {}
+
+  @Post('registro')
+  async registro(
+    @Body() body: {
+      nombre: string;
+      edad: number;
+      correo: string;
+      contrasena: string;
+      nivel_educativo?: string;
+    }
+  ) {
+    return this.usuariosService.registro(body);
+  }
+
+  @Post('login')
+  async login(
+    @Body() body: {
+      correo: string;
+      contrasena: string;
+    }
+  ) {
+    return this.usuariosService.login(body.correo, body.contrasena);
+  }
+}
